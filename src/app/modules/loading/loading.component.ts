b2a47afe4b3f4dae2ed08db8f54754c712d45cf4
pawellesniowski <pawel.html>
2021-06-30 13:14:39 +0200

    4: Add loading service

--- src/app/modules/loading/loading.component.ts
+++ src/app/modules/loading/loading.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { LoadingService } from './loading.service';
 
 @Component({
   selector: 'app-loading',
   templateUrl: './loading.component.html',
   styleUrls: ['./loading.component.scss'],
+  providers: [LoadingService],
 })
 export class LoadingComponent implements OnInit {
-  constructor() {}
+  constructor(
+    @Inject(LoadingService) private readonly loadingService: LoadingService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.loadingService.load().subscribe((v) => console.log('BANG: ', v));
+  }
 }
--- src/app/modules/loading/loading.module.ts
+++ src/app/modules/loading/loading.module.ts
@@ -2,14 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoadingComponent } from './loading.component';
 
-
-
 @NgModule({
-  declarations: [
-    LoadingComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [LoadingComponent],
+  imports: [CommonModule],
+  exports: [LoadingComponent],
 })
-export class LoadingModule { }
+export class LoadingModule {}
