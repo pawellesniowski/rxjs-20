8371c7a2205a361caf1f4126fbd7c9a05e899eca
pawellesniowski <pawel.html>
2021-07-06 14:35:45 +0200

    6: Seats selection solution ONE working

--- src/app/modules/cinema-seats/cinema-seats.component.ts
+++ src/app/modules/cinema-seats/cinema-seats.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable, of, Subject } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cinema-seats',
@@ -8,7 +10,40 @@ import { Component, OnInit } from '@angular/core';
 export class CinemaSeatsComponent implements OnInit {
   readonly seats: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+  private selectedSeats = new BehaviorSubject<Array<number>>([]);
+
+  finalCost = this.selectedSeats.pipe(map((seats) => seats.length * 100));
+
+  selectedSeats$ = this.selectedSeats.asObservable();
+
+  showFinalCosts = false;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  selectSeat(seat: number): void {
+    if (!this.selectedSeats.value.includes(seat)) {
+      this.selectedSeats.next([...this.selectedSeats.value, seat]);
+    } else {
+      this.removeSeat(seat);
+    }
+  }
+
+  removeSeat(seat: number): void {
+    const filtered = this.selectedSeats.value.filter((val) => val !== seat);
+    this.selectedSeats.next(filtered);
+  }
+
+  trackByFn(index: number, item: number): number {
+    return item;
+  }
+
+  isSelected(seat: number): boolean {
+    return this.selectedSeats.value.includes(seat);
+  }
+
+  buyTickets(): void {
+    this.showFinalCosts = true;
+  }
 }
