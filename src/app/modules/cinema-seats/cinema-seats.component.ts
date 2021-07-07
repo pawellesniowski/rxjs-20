07f6a6cc1c09516bbaa51952cbb19710b72ce56c
pawellesniowski <pawel.html>
2021-07-07 12:33:39 +0200

    6: Seats selection solution TWO BEST

--- src/app/modules/cinema-seats/cinema-seats.component.ts
+++ src/app/modules/cinema-seats/cinema-seats.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import { BehaviorSubject, Observable, of, Subject } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { map, scan, share, startWith, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cinema-seats',
@@ -10,38 +10,33 @@ import { filter, map } from 'rxjs/operators';
 export class CinemaSeatsComponent implements OnInit {
   readonly seats: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  private selectedSeats = new BehaviorSubject<Array<number>>([]);
+  readonly selectedSeat: Subject<number> = new Subject<number>();
 
-  finalCost = this.selectedSeats.pipe(map((seats) => seats.length * 100));
+  readonly selectedSeats$ = this.selectedSeat.pipe(
+    scan((acc: Set<number>, cur: number) => {
+      if (acc.has(cur)) {
+        acc.delete(cur);
+      } else {
+        acc.add(cur);
+      }
+      return acc;
+    }, new Set<number>()),
+    startWith(new Set()),
+    share()
+  );
 
-  selectedSeats$ = this.selectedSeats.asObservable();
+  readonly countSelectedSeats$ = this.selectedSeats$.pipe(
+    map((set) => set.size),
+    tap((seats) => this.finalCostSubject.next(seats * 100))
+  );
+
+  finalCostSubject = new BehaviorSubject<number>(0);
 
   showFinalCosts = false;
 
   constructor() {}
 
   ngOnInit(): void {}
-
-  selectSeat(seat: number): void {
-    if (!this.selectedSeats.value.includes(seat)) {
-      this.selectedSeats.next([...this.selectedSeats.value, seat]);
-    } else {
-      this.removeSeat(seat);
-    }
-  }
-
-  removeSeat(seat: number): void {
-    const filtered = this.selectedSeats.value.filter((val) => val !== seat);
-    this.selectedSeats.next(filtered);
-  }
-
-  trackByFn(index: number, item: number): number {
-    return item;
-  }
-
-  isSelected(seat: number): boolean {
-    return this.selectedSeats.value.includes(seat);
-  }
 
   buyTickets(): void {
     this.showFinalCosts = true;
