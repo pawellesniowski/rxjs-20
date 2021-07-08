73d80238d84a439b41fdea7381a7a7b39ce7e183
pawellesniowski <pawel.html>
2021-07-08 10:45:05 +0200

    7: working solution 1

--- src/app/modules/sticky-header/sticky-header.component.ts
+++ src/app/modules/sticky-header/sticky-header.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { BehaviorSubject, fromEvent, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sticky-header',
   templateUrl: './sticky-header.component.html',
-  styleUrls: ['./sticky-header.component.scss']
+  styleUrls: ['./sticky-header.component.scss'],
 })
-export class StickyHeaderComponent implements OnInit {
+export class StickyHeaderComponent implements OnInit, OnDestroy {
+  subscription: Subscription = new Subscription();
+  lastScrollTop = 0;
+  isHidden = new BehaviorSubject(false);
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.subscription = fromEvent(document, 'scroll').subscribe((e) => {
+      const st = window.pageYOffset || document.documentElement.scrollTop;
+      if (st > this.lastScrollTop) {
+        this.isHidden.next(true);
+      } else {
+        this.isHidden.next(false);
+      }
+      this.lastScrollTop = st <= 0 ? 0 : st;
+    });
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
